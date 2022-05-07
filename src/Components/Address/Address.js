33915b63788bf495a1f45a421109a15da41c51f8
somi-kumari <somisourabh96@gmail.com>
2022-05-07 13:02:37 +0530

    updated

--- src/Components/Address/Address.js
+++ src/Components/Address/Address.js
@@ -6,7 +6,20 @@ import {BsTelephone} from "react-icons/bs"
 import {HiOutlineMailOpen} from "react-icons/hi"
 import {AiFillGithub , AiFillLinkedin} from "react-icons/ai"
 function Address() {
+  const mobileRedirect=()=>{
+     window.location.href = "tel:+91-9074363280";
+  }
+  const gmailRedirect=()=>{
+     window.location.href = "mailto:somisourabh96@gmail.com";
+  }
+  const githubRedirect=()=>{
+     window.location.href = "https://github.com/somi-kumari";
+  }
+   const linkedinRedirect=()=>{
+     window.location.href = "https://www.linkedin.com/in/somikumari2010/";
+  }
   return (
+
     <Container>
       <h1 className="contact-address">Contact
         <span className="ctn"> Me</span>
@@ -15,22 +28,23 @@ function Address() {
       <p style={{ textAlign: "center",padding:"20px", color:"white" }}>Whether you want to get in touch, talk about a project collaboration, or just say hi, I'd love to hear from you.</p>
 
       <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col onClick={mobileRedirect} xs={4} md={2} className="tech-icons">
             <BsTelephone/>
             <br/>
-            <span>9074363280</span>
+            {/* <span>+91-9074363280</span> */}
+             <a style={{textDecoration: 'none',color: 'white'}}  href="tel:+919074363280">+919074363280</a>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col onClick={gmailRedirect} xs={4} md={2} className="tech-icons">
             <HiOutlineMailOpen/>
            <br/>
             <a style={{textDecoration: 'none',color: 'white'}}  href="mailto:somisourabh96@gmail.com">somisourabh96@gmail.com</a>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col  onClick={githubRedirect} xs={4} md={2} className="tech-icons">
             <AiFillGithub/>
             <br/>
             <a style={{textDecoration: 'none',color: 'white'}}  href="https://github.com/somi-kumari" target="_blank" rel="noopener noreferrer">Github</a>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col  onClick={linkedinRedirect} xs={4} md={2} className="tech-icons">
             <AiFillLinkedin/>
             <br/>
             
